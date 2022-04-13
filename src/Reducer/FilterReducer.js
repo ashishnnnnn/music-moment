a04becd089f291dab2b4c30ee79ca8f31aca528e
ASHISH KUMAR <40332678+ashishnnnnn@users.noreply.github.com>
2022-04-13 15:54:51 +0530

    reduced the redudency

--- src/Reducer/FilterReducer.js
+++ src/Reducer/FilterReducer.js
@@ -1,7 +1,5 @@
 export function FilterReducer(state, action) {
   const action_type = action.type.toUpperCase();
-  console.log(action_type);
-  console.log(action);
   switch (action_type) {
     case "CATEGORY":
       return { ...state, category: action.payload };
